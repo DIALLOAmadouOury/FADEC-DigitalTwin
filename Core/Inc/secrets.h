b1728b6819f2a700a1e33952ef4241356fa672e4
Amadou Oury DIALLO <ouryamadou334@gmail.com>
2026-05-05 19:36:14 +0200

    Ajout du wifi

---
+++ Core/Inc/secrets.h
@@ -0,0 +1,11 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+/* 
+ * Remplace les valeurs ci-dessous par tes vrais identifiants.
+ * Attention : Respecte bien les majuscules/minuscules !
+ */
+#define WIFI_SSID     "Aod"
+#define WIFI_PASSWORD "Diallo@2"
+
+#endif /* SECRETS_H */
